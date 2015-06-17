dd53199fd246292f5284588d63629618eef85448
Wei Sun <sagasw@gmail.com>
2015-06-17 21:07:01 +0800

    add var

---
+++ codes/var_4.js
@@ -0,0 +1,14 @@
+
+function foo(a) {
+    console.log( a + b ); // if a is number, result is NaN
+    var b = a;
+}
+
+foo( '2' );
+
+function foo2(a) {
+    console.log( a + c ); // c is undefined, exception
+    c = a;
+}
+
+foo2( '2' );
